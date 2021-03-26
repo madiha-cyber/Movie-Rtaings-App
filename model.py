e2aeea17062a28da3ecb2b594b3220068ab3fae9
Madiha <madihagoheer@outlook.com>
2021-03-26 01:07:29 +0000

    Complete Part 2
    co-authored by:  Stephanie Ogamba <stephogam@gmail.com>

--- model.py
+++ model.py
@@ -2,6 +2,8 @@
 
 from flask_sqlalchemy import SQLAlchemy
 
+from datetime import datetime
+
 db = SQLAlchemy()
 
 
@@ -15,6 +17,8 @@ class User(db.Model):
                         primary_key=True)
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
+
+    # ratings = a list of Rating objects
 
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email}>'
@@ -33,6 +37,8 @@ class Movie(db.Model):
     release_date = db.Column(db.DateTime)
     poster_path = db.Column(db.String)
 
+    # ratings = a list of Rating objects
+
     def __repr__(self):
         return f'<Movie movie_id={self.movie_id} title={self.title}>'
 
@@ -47,7 +53,10 @@ class Rating(db.Model):
                         primary_key=True)
     score = db.Column(db.Integer)
     movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    movie = db.relationship('Movie', backref='ratings')
+    user = db.relationship('User', backref='ratings')
 
     def __repr__(self):
         return f'<Rating rating_id={self.rating_id} score={self.score}>'
@@ -56,7 +65,7 @@ class Rating(db.Model):
 
 def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_ECHO'] = False
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.app = flask_app
@@ -79,3 +88,5 @@ if __name__ == '__main__':
     # query it executes.
 
     connect_to_db(app)
+
+    db.create_all()
